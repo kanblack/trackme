9945a8a989e578360d4ac6c422bfab8bb12e83ab
DESKTOP-I0PRB3M\kanbl <kanblack.va@gmail.com>
2020-11-11 21:01:20 +0700

    update display current location

--- trackme-source/app/src/main/java/kb/dev/trackme/map/MapManager.kt
+++ trackme-source/app/src/main/java/kb/dev/trackme/map/MapManager.kt
@@ -1,7 +1,18 @@
 package kb.dev.trackme.map
 
-import com.google.android.gms.maps.SupportMapFragment
+import android.app.Activity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
 
 interface MapManager {
-    fun attachMap(mapFragment: SupportMapFragment)
+    fun attachMap(activity: Activity, googleMap: GoogleMap)
+    fun updateDatePermissionResult(
+        activity: Activity,
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    )
+
+    fun saveMapState(outState: Bundle)
+    fun restoreMapState(savedInstanceState: Bundle)
 }
--- trackme-source/app/src/main/java/kb/dev/trackme/mvvm/viewmodels/SessionViewModel.kt
+++ trackme-source/app/src/main/java/kb/dev/trackme/mvvm/viewmodels/SessionViewModel.kt
@@ -1,19 +1,32 @@
 package kb.dev.trackme.mvvm.viewmodels
 
+import android.app.Activity
+import android.os.Bundle
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kb.dev.trackme.map.MapManager
 import kb.dev.trackme.repositories.SessionRepository
 
 class SessionViewModel(repository: SessionRepository, private val mapManager: MapManager): ViewModel() {
+    fun onAttachMap(activity: Activity, googleMap: GoogleMap) {
+        mapManager.attachMap( activity,googleMap)
+    }
 
-    fun attachMap(mapFragment: SupportMapFragment) {
-        mapManager.attachMap(mapFragment)
+    fun onRequestPermissionsResult(
+        activity: Activity,
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        mapManager.updateDatePermissionResult(activity, requestCode, permissions, grantResults)
+    }
 
+    fun onSaveMapState(outState: Bundle) {
+        mapManager.saveMapState(outState)
+
+    }
+
+    fun onRestoreMapState(savedInstanceState: Bundle) {
+        mapManager.restoreMapState(savedInstanceState)
     }
 }
